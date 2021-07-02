548edb68ebebc0c538d44ac0e8ae652e12af5c29
Burak Can KARAKURT <84005554+jellybeanist@users.noreply.github.com>
2021-07-02 09:53:51 +0300

    Add files via upload
    Definitions of the register addresses.

---
+++ main.h
@@ -0,0 +1,21 @@
+/*
+ * main.h
+ *
+ *  Created on: 1 Tem 2021
+ *      Author: dellworkstation
+ */
+
+#ifndef SRC_MAIN_H_
+#define SRC_MAIN_H_
+
+#define BRAM_WRITE_BASE_ADDRESS XPAR_AXI_BRAM_CTRL_0_S_AXI_BASEADDR
+#define BRAM_READ_BASE_ADDRESS  XPAR_AXI_BRAM_CTRL_0_S_AXI_BASEADDR + 4*(0x800)
+
+#define CLK_DIV_BAUD 	((u32*) (BRAM_WRITE_BASE_ADDRESS + 4*(0x00)))
+#define TX_BUF_DATA 	((u32*) (BRAM_WRITE_BASE_ADDRESS + 4*(0x01)))
+
+#define RX_BUF_EMTY 	((u32*) (BRAM_READ_BASE_ADDRESS + 4*(0x00)))
+#define RX_BUF_DATA 	((u32*) (BRAM_READ_BASE_ADDRESS + 4*(0x01)))
+#define TX_BUF_FULL 	((u32*) (BRAM_READ_BASE_ADDRESS + 4*(0x02)))
+
+#endif /* SRC_MAIN_H_ */
