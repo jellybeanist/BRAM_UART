c86f816732b245a8e30e4c7de3ca0fcb635b6e7b
Burak Can KARAKURT <84005554+jellybeanist@users.noreply.github.com>
2021-07-02 12:25:50 +0300

    Update main.c
    Generic "Clock Frequency" and "Baud Rate" variables on TX and RX modules removed. Now I can change that in Microblaze. Also, TX; RX and TRX modules have changed.

--- main.c
+++ main.c
@@ -4,13 +4,16 @@
 
 int main (void)
 {
+	CLK_DIV_BAUD[0] = 100000000/115200;
+	sleep(1);
 	while (1)
 	{
-		
+
 		if(RX_BUF_EMTY[0] == 0)
 			TX_BUF_DATA[0] = RX_BUF_DATA[0];
-		
-		sleep(1);
+
+
+
 	}
 
 	return 0;
