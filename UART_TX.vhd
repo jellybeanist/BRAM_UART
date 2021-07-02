4de24a7119df8d6a859736058e832a40f77e52a0
Burak Can KARAKURT <84005554+jellybeanist@users.noreply.github.com>
2021-07-02 12:28:04 +0300

    Update UART_TX.vhd
    Generic "Clock Frequency" and "Baud Rate" variables on TX and RX modules removed. Now I can change that in Microblaze. Also, TX; RX and TRX modules have changed.

--- UART_TX.vhd
+++ UART_TX.vhd
@@ -1,104 +1,103 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all ;
+use ieee.std_logic_unsigned.all ;
 
 entity UART_TX is
-    generic 
-    (  
-        CLK_FREQ            :   INTEGER := 100_000_000;
-        BAUD_RATE           :   INTEGER := 115_200
-    );
-    port (
-        CLK                 :   IN STD_LOGIC;           
-        RST_IN              :   IN STD_LOGIC;     
-                       
-        DATA_IN             :   IN STD_LOGIC_VECTOR (7 DOWNTO 0); 
-        TX_START_IN         :   IN STD_LOGIC; 
-                            
-        TX_BUSY             :   OUT STD_LOGIC;
-        TX_OUT              :   OUT STD_LOGIC;                    
-        TX_DONE_OUT         :   OUT STD_LOGIC                     
-    );
+	port 
+	( 
+		CLK 			: IN  	STD_LOGIC;
+        RST 			: IN  	STD_LOGIC;
+		
+        TX_OUT 			: OUT	STD_LOGIC;
+		
+		CLK_DIV_BAUD	: IN	STD_LOGIC_VECTOR(31 DOWNTO 0);
+		
+        TX_DATA 		: IN  	STD_LOGIC_VECTOR(7 DOWNTO 0);
+        TX_DATA_VALID 	: IN  	STD_LOGIC;
+        TX_BUSY			: OUT  	STD_LOGIC
+	);
 end UART_TX;
 
 architecture Behavioral of UART_TX is
 
-type states is (IDLE, START, TRANSFER, STOP);
-signal state                :   states := IDLE; 
-constant clock_count_lim    :   integer := CLK_FREQ/BAUD_RATE+1;  
-
-signal data                 :   std_logic_vector(7 downto 0) := (others=>'0');
-signal clock_counter        :   integer range 0 to (clock_count_lim)-1 := 0;
-signal data_index           :   integer range 0 to 7 := 0;
-signal tx                   :   std_logic := '1';
-signal i_tx_busy            :   std_logic;
-signal tx_done              :   std_logic := '0';
-
+	signal baudCounter			: integer := 0;
+	signal bitCounter			: integer range 0 to 8 := 0;
+	signal tx_data_sreg			: std_logic_vector(7 downto 0) := (others=>'0');
+	signal state				: integer range 0 to 3 := 0;
+    
+    signal i_tx_busy            : std_logic;
 
 begin
-    TX_OUT      <=  tx;
-    TX_DONE_OUT <=  tx_done;
-    TX_BUSY     <=  i_tx_busy;
 
-MAIN :  process (CLK, RST_IN) begin
-        
-            if (RST_IN = '1') then
-                state <= IDLE;
-                clock_counter <= 0;
-                data_index <= 0;
-                data <= (others=>'0');
-                tx <= '1';
-                tx_done <= '0';
-        
-            elsif rising_edge(CLK) then
-            i_tx_busy <= '1';
-                case state is
-                    when IDLE =>
-                        tx  			<= '1'; 
-                        tx_done	        <= '0';
-                        clock_counter	<= 0;
-                        i_tx_busy <= '0';
-                        if (TX_START_IN = '1') then
-                            state       <= START;
-                            data        <= data_in;
-                            tx          <= '0';
-                            i_tx_busy   <= '1';
-                        end if;
-                
-                    when START =>
-                    -- tx_out is equal to 0 when program enters here.
-                        if (clock_counter = clock_count_lim-1) then
-                            state           <= TRANSFER; 
-                            clock_counter	<= 0;
-                        else
-                            clock_counter	<= clock_counter + 1;
-                        end if;
-                            
-                    when TRANSFER =>
-                        tx <= data(data_index);
-                        if (clock_counter = clock_count_lim-1) then
-                            clock_counter   <= 0;
-                            if (data_index = 7) then
-                                data_index  <= 0;
-                                state       <= STOP;
-                            else
-                                data_index  <= data_index+1;
-                            end if;
-                        else
-                            clock_counter   <= clock_counter+1;
-                        end if;		
+    tx_busy <= i_tx_busy; -- or tx_data_valid;
+
+	state_machine_p : process(clk)
+	begin
+		if(rising_edge(clk)) then
+			
+			i_tx_busy <= '1';
+
+			case state is
+			
+				-- idle
+				when 0 =>
+					tx_out <= '1';
+					i_tx_busy <= '0';
+					baudCounter <= 0;
+					bitCounter <= 0;
+					if(tx_data_valid='1' and CLK_DIV_BAUD>0) then
+						tx_data_sreg <= tx_data;
+						tx_out <= '0';
+						baudCounter <= 1;
+                        i_tx_busy <= '1';
+						state <= 1;
+					end if;
+					
+				-- send start bit
+				when 1 =>
+					if(baudCounter>=conv_integer(clk_div_baud)) then
+						baudCounter <= 1;
+						tx_out <= tx_data_sreg(bitCounter);
+						bitCounter <= bitCounter + 1;
+						baudCounter <= 1;
+						state <= 2;
+					else
+						baudCounter <= baudCounter + 1;
+					end if;
+					
+				-- send data bits
+				when 2 =>
+					if(baudCounter>=conv_integer(clk_div_baud)) then
+						if(bitCounter>=8) then
+							tx_out <= '1';
+							state <= 3;
+						else
+							tx_out <= tx_data_sreg(bitCounter);
+						end if;
+						bitCounter <= bitCounter + 1;
+						baudCounter <= 1;
+					else
+						baudCounter <= baudCounter + 1;
+					end if;
+					
+				-- send stop bits
+				when 3 =>
+					if(baudCounter>=conv_integer(clk_div_baud)) then
+						state <= 0;
+					else
+						baudCounter <= baudCounter + 1;
+					end if;
+					
+                when others =>
+                    state <= 0;
                     
-                    when STOP =>
-                        tx <= '1';		
-                        if (clock_counter = clock_count_lim-1) then
-                            state		    <= IDLE;
-                            tx_done		    <= '1';
-                        else
-                            clock_counter	<= clock_counter + 1;				
-                        end if;		
-                end case;
-            end if;
-        end process;
-
+			end case;
+			
+			if(rst='1') then
+				state <= 0;
+				tx_out <= '0';
+			end if;
+		end if;
+	end process;
 end Behavioral;
